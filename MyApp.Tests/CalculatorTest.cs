73b86cd614284fe8b346302ada34a7c1d2208482
Anup-Rajendra <anupsargur2@gmail.com>
2024-09-02 10:38:55 +0530

    Add Tests folder

---
+++ MyApp.Tests/CalculatorTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyApp;
+namespace MyApp.Tests
+{
+    public class CalculatorTest
+    {
+        [Test]
+        public void Add_Test()
+        {
+            Calculator calculator = new Calculator();
+            int result = calculator.Add(2, 3);
+            Assert.AreEqual(5, result);
+
+        }
+    }
+}
